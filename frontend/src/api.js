dbff1ace95a4d68b276547cf16f8e23bc928c4be
Yoyo1029 <YoyoWu911029@gmail.com>
2022-12-29 20:14:22 +0800

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
-    ? "/"
-    : "http://localhost:4000/";
+    ? "/api"
+    : "http://localhost:4000/api";
 
-// const api = axios.create({ baseURL: API_ROOT });
+const api = axios.create({ baseURL: API_ROOT });
 
-// export default api;
-export default axios.create({ baseURL: API_ROOT });
+export default api;
+// export default axios.create({ baseURL: API_ROOT });
 
 // instance.get('/hi').then((data) => console.log(data));
